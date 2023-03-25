403b231232f37cf68a505e9a9c2ea3f6e5151f67
Saif El-Din Hazem <ivandorix@gmail.com>
2023-03-25 14:48:02 +0200

    Header File

---
+++ txtheader.h
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+
+using namespace std;
+
+char fileName[81] ,secondName[81] ,line[100];
+vector<string> text;
+
+
+
+
+// Get name of the file from the user.
+void getName();
+
+//Adding text at the end of the given file.
+void addTxt();
+
+// Display the given file content.
+void displayContent();
+
+//Clears given file content
+void emptyFile();
+
+// Encrypt the data of the given  file.
+void encryptFile();
+
+// Decrypt the data of the given file.
+void decryptFile();
+
+// Merge the data of the two given files.
+void mergeFile();
+
+// Count words in the given file.
+void cntWords();
+
+// Count characters in the given file.
+void cntChar();
+
+// Count lines in the given file.
+void cntLines();
+
+//Search for a specific word in the given file.
+void searchWord();
+
+//Count the number of times a word exists in the given file.
+void cntExistence();
+
+//Turn the content to upper case
+void upperCase();
+
+//Turn the content to lower case
+void lowerCase();
+
+//Turn file content to first caps
+void firstCaps();
+
+//Save Program
+void save();
+
